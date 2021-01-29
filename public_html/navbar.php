550c00dbfac15844b41f335befd6e3ac02446883
Danny Cheng <a7932677@gmail.com>
2021-01-29 16:23:56 +0800

    + navbar

---
+++ public_html/navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Calendar</a>
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Sign IN</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Sign Up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ public_html/navbar_user.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Calendar</a>
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <?php session_start();
+                        echo $_SESSION['name'] ?></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Log Out</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
