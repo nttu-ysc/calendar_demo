ee00129dc09bb75317e7cca1e8e972ce0ca7b8ff
Danny Cheng <a7932677@gmail.com>
2021-01-29 16:39:11 +0800

    + signin AJAX call + signin method

--- public_html/js/user.js
+++ public_html/js/user.js
@@ -13,4 +13,19 @@ $(document).ready(function () {
                 }
             });
     });
+    $('#signIn form').on('submit', function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        var action = 'user/signin.php';
+        $.post(action, data)
+            .done(function (data) {
+                console.log('success');
+                window.location.replace('index.php', data);
+            })
+            .fail(function (jqXHR) {
+                if (jqXHR.responseText !== '') {
+                    $('.errors').addClass('open').find('li').text(jqXHR.responseText);
+                }
+            });
+    });
 });
